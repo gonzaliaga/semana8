69d7f67e4ac7669e27b2b8851ee6f3fdec29b633
Gonzalo Cuellar Aliaga <111820928+gonzaliaga@users.noreply.github.com>
2025-05-03 13:11:20 -0400

    Update ejercicio4.js

--- ejercicio4.js
+++ ejercicio4.js
@@ -9,7 +9,7 @@ function agregarPaciente(listaPacientes, nuevoPaciente){
     if(existe){
         throw new Error ('El paciente ya existe en la lista');
     }
-    //retornar una nueva lista con el nuevo paciente agregado porsiaca
+    //retornar una nueva lista con el nuevo paciente agregado porsiaca blblablabla
     return [...listaPacientes, nuevoPaciente];
 }
 
